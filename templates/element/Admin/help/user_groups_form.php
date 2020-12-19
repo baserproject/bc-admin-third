e8df723317d8b2273c56e8bdba43bcc4837c6619
ryuring <ryuring55@gmail.com>
2020-12-19 13:55:16 +0900

    ヘルプを実装

---
+++ templates/element/Admin/help/user_groups_form.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * baserCMS :  Based Website Development Project <https://basercms.net>
+ * Copyright (c) baserCMS User Community <https://basercms.net/community/>
+ *
+ * @copyright     Copyright (c) baserCMS User Community
+ * @link          https://basercms.net baserCMS Project
+ * @since         5.0.0
+ * @license       http://basercms.net/license/index.html MIT License
+ */
+
+/**
+ * [ADMIN] ユーザーグループ編集　ヘルプ
+ */
+?>
+
+
+<p><?php echo __d('baser', 'ログインするユーザーのグループを登録します。コンテンツへの権限内容がわかりやすい名称で登録します。')?></p>
